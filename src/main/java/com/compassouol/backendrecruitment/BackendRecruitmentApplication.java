536d1775c2b2bd5170e31bc97a0ba13af5ef82e9
Eduardo Spagna <eduardo@gempe.dev>
2021-02-13 17:40:52 -0300

    First commit

---
+++ src/main/java/com/compassouol/backendrecruitment/BackendRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.compassouol.backendrecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRecruitmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compassouol/backendrecruitment/BackendRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compassouol.backendrecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
