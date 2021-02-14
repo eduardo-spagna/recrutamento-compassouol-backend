19f99d5cedefdab1f2b763a2337f498bfc24cb97
Eduardo Spagna <eduardo@gempe.dev>
2021-02-14 00:02:35 -0300

    Add State model

---
+++ src/main/java/com/compassouol/backendrecruitment/models/State.java
@@ -0,0 +1,27 @@
+package com.compassouol.backendrecruitment.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "states")
+@Data
+public class State implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "state_id")
+    private long stateId;
+
+    @Column(name = "state_name")
+    private String stateName;
+
+    @Column(name = "state_short_name")
+    private String stateShortName;
+}
