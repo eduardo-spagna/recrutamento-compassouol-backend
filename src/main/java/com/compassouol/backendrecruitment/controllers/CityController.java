cea9f46ffd828b7f508ded0c1a44d9abd74b0a03
Eduardo Spagna <eduardo@gempe.dev>
2021-02-14 02:22:07 -0300

    Add endpoint to list all cities with possibility to search by city or state name

--- src/main/java/com/compassouol/backendrecruitment/controllers/CityController.java
+++ src/main/java/com/compassouol/backendrecruitment/controllers/CityController.java
@@ -1,10 +1,14 @@
 package com.compassouol.backendrecruitment.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import com.compassouol.backendrecruitment.dtos.request.city.CreateCityRequestDTO;
 import com.compassouol.backendrecruitment.dtos.response.ResponseDTO;
 import com.compassouol.backendrecruitment.dtos.response.city.CreateCityResponseDTO;
+import com.compassouol.backendrecruitment.dtos.response.city.ShowCityResponseDTO;
 import com.compassouol.backendrecruitment.dtos.response.state.ShowStateResponseDTO;
 import com.compassouol.backendrecruitment.models.City;
 import com.compassouol.backendrecruitment.models.State;
@@ -15,9 +19,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
@@ -60,6 +66,32 @@ public class CityController {
             e.printStackTrace();
             ResponseDTO<CreateCityResponseDTO> response = new ResponseDTO<CreateCityResponseDTO>("city/creating-error",
                     "Ocorreu um erro ao criar a cidade", null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @GetMapping
+    @ApiOperation(value = "List all cities (optional: search by city or state name)")
+    public ResponseEntity<ResponseDTO<List<ShowCityResponseDTO>>> listAll(
+            @RequestParam(value = "search", required = false) String search) {
+        try {
+            List<City> cities = cityService.findAllWithSearch(search);
+
+            List<ShowCityResponseDTO> showCityListDTO = new ArrayList<>();
+            for (City city : cities) {
+                showCityListDTO.add(new ShowCityResponseDTO(city.getCityId(), city.getCityName(),
+                        new ShowStateResponseDTO(city.getState().getStateId(), city.getState().getStateName(),
+                                city.getState().getStateShortName())));
+            }
+
+            ResponseDTO<List<ShowCityResponseDTO>> response = new ResponseDTO<List<ShowCityResponseDTO>>(
+                    "city/successfully-listed", "Listagem de cidades realizada com sucesso", showCityListDTO);
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            ResponseDTO<List<ShowCityResponseDTO>> response = new ResponseDTO<List<ShowCityResponseDTO>>(
+                    "city/listing-error", "Ocorreu um erro ao listar as cidades", null);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
