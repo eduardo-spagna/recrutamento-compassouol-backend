03834d1e0afc741a4339e5fce25cb279f8e2f0ab
Eduardo Spagna <eduardo@gempe.dev>
2021-02-14 05:55:10 -0300

    Add default timezone to the project

--- src/main/java/com/compassouol/backendrecruitment/BackendRecruitmentApplication.java
+++ src/main/java/com/compassouol/backendrecruitment/BackendRecruitmentApplication.java
@@ -1,10 +1,18 @@
 package com.compassouol.backendrecruitment;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendRecruitmentApplication {
+	@PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendRecruitmentApplication.class, args);
