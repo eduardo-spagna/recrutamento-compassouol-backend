f6fe5d92d533e70cf8336ade5f6c34cc6bd8bce6
Eduardo Spagna <eduardo@gempe.dev>
2021-02-14 20:42:05 -0300

    Add @AllArgsConstructor e @NoArgsConstructor in the City model

--- src/main/java/com/compassouol/backendrecruitment/models/City.java
+++ src/main/java/com/compassouol/backendrecruitment/models/City.java
@@ -12,11 +12,15 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "cities")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class City implements Serializable {
     private static final long serialVersionUID = 1L;
 
