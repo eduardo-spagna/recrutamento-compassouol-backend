b258cdc71088a1eb139cc750875c41b1b92397bd
Eduardo Spagna <eduardo@gempe.dev>
2021-02-14 05:10:19 -0300

    Add DTO to show customer data

---
+++ src/main/java/com/compassouol/backendrecruitment/dtos/response/customer/ShowCustomerResponseDTO.java
@@ -0,0 +1,20 @@
+package com.compassouol.backendrecruitment.dtos.response.customer;
+
+import java.time.LocalDate;
+
+import com.compassouol.backendrecruitment.dtos.response.city.ShowCityResponseDTO;
+import com.compassouol.backendrecruitment.dtos.response.gender.ShowGenderResponseDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ShowCustomerResponseDTO {
+    private long customerId;
+    private String customerName;
+    private LocalDate customerBirthdate;
+    private int customerAge;
+    private ShowGenderResponseDTO gender;
+    private ShowCityResponseDTO city;
+}
