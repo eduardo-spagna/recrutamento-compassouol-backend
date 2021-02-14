91d4d9c60a99a0d29bd8a4ac9a4e1fe9f132c717
Eduardo Spagna <eduardo@gempe.dev>
2021-02-14 04:45:28 -0300

    Add new attributes to the Customer model

--- src/main/java/com/compassouol/backendrecruitment/models/Customer.java
+++ src/main/java/com/compassouol/backendrecruitment/models/Customer.java
@@ -2,9 +2,12 @@ package com.compassouol.backendrecruitment.models;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -19,14 +22,24 @@ public class Customer implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customer_id")
     private long customerId;
 
     @Column(name = "customer_name")
     private String customerName;
 
+    @Column(name = "customer_name_normalized")
+    private String customerNameNormalized;
+
     @Column(name = "customer_birthdate")
     private LocalDate customerBirthdate;
+
+    @Column(name = "customer_created_at", updatable = false)
+    private LocalDateTime customerCreatedAt = LocalDateTime.now();
+
+    @Column(name = "customer_updated_at")
+    private LocalDateTime customerUpdatedAt = LocalDateTime.now();
 
     @ManyToOne
     @JoinColumn(name = "gender_id")
