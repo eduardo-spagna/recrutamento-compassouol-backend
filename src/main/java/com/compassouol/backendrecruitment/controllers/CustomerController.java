b608c32a6dd9da0f795b0a4e30473e274dfa5d94
Eduardo Spagna <eduardo@gempe.dev>
2021-02-14 05:11:15 -0300

    Add endpoint to list all customers with possibility to search by customer name

--- src/main/java/com/compassouol/backendrecruitment/controllers/CustomerController.java
+++ src/main/java/com/compassouol/backendrecruitment/controllers/CustomerController.java
@@ -1,11 +1,15 @@
 package com.compassouol.backendrecruitment.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import com.compassouol.backendrecruitment.dtos.request.customer.CreateCustomerRequestDTO;
 import com.compassouol.backendrecruitment.dtos.response.ResponseDTO;
 import com.compassouol.backendrecruitment.dtos.response.city.ShowCityResponseDTO;
 import com.compassouol.backendrecruitment.dtos.response.customer.CreateCustomerResponseDTO;
+import com.compassouol.backendrecruitment.dtos.response.customer.ShowCustomerResponseDTO;
 import com.compassouol.backendrecruitment.dtos.response.gender.ShowGenderResponseDTO;
 import com.compassouol.backendrecruitment.dtos.response.state.ShowStateResponseDTO;
 import com.compassouol.backendrecruitment.models.City;
@@ -20,9 +24,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
@@ -82,6 +88,43 @@ public class CustomerController {
             e.printStackTrace();
             ResponseDTO<CreateCustomerResponseDTO> response = new ResponseDTO<CreateCustomerResponseDTO>(
                     "customer/creating-error", "Ocorreu um erro ao criar o cliente", null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @GetMapping
+    @ApiOperation(value = "List all customers (optional: search by customer name)")
+    public ResponseEntity<ResponseDTO<List<ShowCustomerResponseDTO>>> listAll(
+            @RequestParam(value = "search", required = false) String search) {
+        try {
+            List<Customer> customers = customerService.findAllWithSearch(search);
+
+            List<ShowCustomerResponseDTO> showCustomerListDTO = new ArrayList<>();
+            for (Customer customer : customers) {
+                int customerAge = DateUtil.getAgeFromBirthdate(customer.getCustomerBirthdate());
+
+                ShowGenderResponseDTO showGenderResponseDTO = new ShowGenderResponseDTO(
+                        customer.getGender().getGenderId(), customer.getGender().getGenderDescription());
+
+                ShowCityResponseDTO showCityResponseDTO = new ShowCityResponseDTO(customer.getCity().getCityId(),
+                        customer.getCity().getCityName(),
+                        new ShowStateResponseDTO(customer.getCity().getState().getStateId(),
+                                customer.getCity().getState().getStateName(),
+                                customer.getCity().getState().getStateShortName()));
+
+                showCustomerListDTO.add(new ShowCustomerResponseDTO(customer.getCustomerId(),
+                        customer.getCustomerName(), customer.getCustomerBirthdate(), customerAge, showGenderResponseDTO,
+                        showCityResponseDTO));
+            }
+
+            ResponseDTO<List<ShowCustomerResponseDTO>> response = new ResponseDTO<List<ShowCustomerResponseDTO>>(
+                    "customer/successfully-listed", "Listagem de clientes realizada com sucesso", showCustomerListDTO);
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            ResponseDTO<List<ShowCustomerResponseDTO>> response = new ResponseDTO<List<ShowCustomerResponseDTO>>(
+                    "customer/listing-error", "Ocorreu um erro ao listar os clientes", null);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
