04aa4d639f7dd87b654324ebe32dc287929913e0
Eduardo Spagna <eduardo@gempe.dev>
2021-02-14 00:08:57 -0300

    Add StateRepository

---
+++ src/main/java/com/compassouol/backendrecruitment/repositories/StateRepository.java
@@ -0,0 +1,9 @@
+package com.compassouol.backendrecruitment.repositories;
+
+import com.compassouol.backendrecruitment.models.State;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+    
+}
